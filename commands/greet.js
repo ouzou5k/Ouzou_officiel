2ed9e54742f27d17f3516a1203ea569bb91baaef
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:10:24 +0000

    Update greet.js

--- commands/greet.js
+++ commands/greet.js
@@ -1,1 +1,8 @@
-
+export default {
+  name: "greet",
+  description: "Envoie un message de bienvenue",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    await sock.sendMessage(m.key.remoteJid, { text: "👋 Bienvenue sur le groupe !" }, { quoted: m });
+  }
+};
