a5325ccaea6289f81b90c298c08cd516dcfe38f9
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:11:37 +0000

    Update covid.js

--- commands/covid.js
+++ commands/covid.js
@@ -1,1 +1,9 @@
-
+export default {
+  name: "covid",
+  description: "Infos COVID (Mali exemple)",
+  category: "info",
+  execute: async (sock, m, args) => {
+    const msg = "🦠 COVID-19 Mali : 35 000 cas, 1 000 décès (exemple)";
+    await sock.sendMessage(m.key.remoteJid, { text: msg }, { quoted: m });
+  }
+};
