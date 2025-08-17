b1c7de328d5ce93306292b31194b54d350e8073e
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:02:52 +0000

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,1 +1,11 @@
 
+export default {
+  name: "meme",
+  description: "Envoie un meme aléatoire",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const memes = ["😂 Meme1", "🤣 Meme2", "😎 Meme3"];
+    const meme = memes[Math.floor(Math.random() * memes.length)];
+    await sock.sendMessage(m.key.remoteJid, { text: meme }, { quoted: m });
+  }
+};
