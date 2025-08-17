5b77ad3349e24c02ddb8b07c15c204eeceab2bc2
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:06:47 +0000

    Update hack.js

--- commands/hack.js
+++ commands/hack.js
@@ -1,1 +1,11 @@
-
+export default {
+  name: "hack",
+  description: "Jeu de simulation hack",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const cible = args[0] || "utilisateur";
+    const pourcent = Math.floor(Math.random() * 101);
+    const msg = `💻 Hacking ${cible} ... ${pourcent}% terminé 🕵️‍♂️`;
+    await sock.sendMessage(m.key.remoteJid, { text: msg }, { quoted: m });
+  }
+};
