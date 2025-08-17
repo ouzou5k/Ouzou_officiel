0946cd3cb09ea129ed2083602431a36196f1d411
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:10:57 +0000

    Update goodbye.js

--- commands/goodbye.js
+++ commands/goodbye.js
@@ -1,1 +1,8 @@
-
+export default {
+  name: "goodbye",
+  description: "Envoie un message d’au revoir",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    await sock.sendMessage(m.key.remoteJid, { text: "👋 Au revoir et à bientôt !" }, { quoted: m });
+  }
+};
