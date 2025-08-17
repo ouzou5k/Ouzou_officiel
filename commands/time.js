e24b7307565273d787bd1028627dfbbdab6a2f9c
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:07:41 +0000

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -1,1 +1,10 @@
-
+export default {
+  name: "time",
+  description: "Affiche l’heure actuelle",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const date = new Date();
+    const msg = `⏰ Heure actuelle : ${date.toLocaleTimeString()}`;
+    await sock.sendMessage(m.key.remoteJid, { text: msg }, { quoted: m });
+  }
+};
