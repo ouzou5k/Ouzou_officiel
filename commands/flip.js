a428742c6910e692ea68d3ac750c5c3a9add9c7a
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:08:34 +0000

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -1,1 +1,9 @@
-
+export default {
+  name: "flip",
+  description: "Lancer une pièce (pile ou face)",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const result = Math.random() < 0.5 ? "Pile" : "Face";
+    await sock.sendMessage(m.key.remoteJid, { text: `🪙 Résultat : ${result}` }, { quoted: m });
+  }
+};
