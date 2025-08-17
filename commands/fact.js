0ded88f60476f62af533a444a0f2c7e0d5da7aee
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:21:37 +0000

    Update fact.js

--- commands/fact.js
+++ commands/fact.js
@@ -1,1 +1,14 @@
-
+export default {
+  name: "fact",
+  description: "Envoie un fait aléatoire",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const facts = [
+      "Saviez-vous ? Les chats ont 32 muscles dans chaque oreille.",
+      "Saviez-vous ? Le soleil est 109 fois plus large que la Terre.",
+      "Saviez-vous ? L’abeille peut reconnaître les visages humains."
+    ];
+    const fact = facts[Math.floor(Math.random() * facts.length)];
+    await sock.sendMessage(m.key.remoteJid, { text: fact }, { quoted: m });
+  }
+};
