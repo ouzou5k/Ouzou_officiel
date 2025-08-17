2a32a35602023d0391915eb146ed3142f4901b61
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 20:50:35 +0000

    Update profil.js

--- commands/profil.js
+++ commands/profil.js
@@ -1,1 +1,10 @@
-
+export default {
+  name: "profil",
+  description: "Affiche les infos du profil",
+  category: "info",
+  execute: async (sock, m, args) => {
+    const user = m.pushName || "Utilisateur";
+    const msg = `👤 Profil de : ${user}`;
+    await sock.sendMessage(m.key.remoteJid, { text: msg }, { quoted: m });
+  }
+};
