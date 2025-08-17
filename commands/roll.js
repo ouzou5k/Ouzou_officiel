ae3f7f34f1b5bd6195dcdb8da9fdfca3776bfaa3
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:09:14 +0000

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,1 +1,9 @@
-
+export default {
+  name: "roll",
+  description: "Lancer un dé",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const result = Math.floor(Math.random() * 6) + 1;
+    await sock.sendMessage(m.key.remoteJid, { text: `🎲 Résultat du dé : ${result}` }, { quoted: m });
+  }
+};
