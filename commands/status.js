c0c44504dd324e72c0b5d06458e0d5644ce56574
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 20:51:48 +0000

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,1 +1,9 @@
-
+export default {
+  name: "status",
+  description: "Affiche le statut du bot",
+  category: "info",
+  execute: async (sock, m, args) => {
+    const msg = "✅ Bot en ligne et fonctionne correctement !";
+    await sock.sendMessage(m.key.remoteJid, { text: msg }, { quoted: m });
+  }
+};
