27192a7d34f1a78411eea613c1ffba50440b0b64
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:09:46 +0000

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,1 +1,10 @@
-
+export default {
+  name: "say",
+  description: "Répète ce que vous écrivez",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const text = args.join(" ");
+    if (!text) return await sock.sendMessage(m.key.remoteJid, { text: "⚠️ Écris quelque chose après .say" }, { quoted: m });
+    await sock.sendMessage(m.key.remoteJid, { text }, { quoted: m });
+  }
+};
