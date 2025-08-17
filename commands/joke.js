6719cadb2f6d448eb80cc1be4315dca7415dbff3
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 19:05:29 +0000

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -1,1 +1,10 @@
-
+export default {
+  name: "joke",
+  description: "Envoie une blague aléatoire",
+  category: "fun",
+  execute: async (sock, m, args) => {
+    const jokes = ["Pourquoi le chat traverse la route ? 🤔", "Blague2", "Blague3"];
+    const joke = jokes[Math.floor(Math.random() * jokes.length)];
+    await sock.sendMessage(m.key.remoteJid, { text: joke }, { quoted: m });
+  }
+};
