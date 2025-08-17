65aeaf74d091a06c64785c26a37071d1d4075a31
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-17 21:16:33 +0000

    Update quiz.js

--- commands/quiz.js
+++ commands/quiz.js
@@ -1,1 +1,14 @@
-
+export default {
+  name: "quiz",
+  description: "Pose une question quiz",
+  category: "jeu",
+  execute: async (sock, m) => {
+    const quizzes = [
+      "❓ Quelle est la capitale du Mali ? (Bamako / Ségou / Mopti)",
+      "❓ Qui a inventé l'électricité ? (Tesla / Edison / Einstein)",
+      "❓ Combien font 7x8 ? (54 / 56 / 58)"
+    ];
+    const random = quizzes[Math.floor(Math.random() * quizzes.length)];
+    await sock.sendMessage(m.key.remoteJid, { text: random }, { quoted: m });
+  }
+};
